fde91abb1bebc31b2e82fb806d4267ae244e665e
Alex Lev <tuswells@gmail.com>
2025-05-31 17:14:02 +0300

    feat: pring_diamond

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 void print_peak(int suite, char value);
 void print_worm(int suite, char value);
 void print_cross(int suite, char value);
+void print_diamond(int suite, char value);
 
 int main(void)
 {
-    print_cross(5, 'A');
+    print_diamond(5, 'A');
 }
 
 // Print peak
@@ -44,6 +45,19 @@ void print_cross(int suite, char value)
     printf("|   (  )   |\n");
     printf("|    ()    |\n");
     printf("|    ||    |\n");
+    printf("|        %c |\n", value);
+    printf("+----------+\n");
+}
+
+// Print diamond
+void print_diamond(int suite, char value)
+{
+    printf("+----------+\n");
+    printf("| %c        |\n", value);
+    printf("|    /\\    |\n");
+    printf("|   /  \\   |\n");
+    printf("|   \\  /   |\n");
+    printf("|    \\/    |\n");
     printf("|        %c |\n", value);
     printf("+----------+\n");
 }
